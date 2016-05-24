7048aefc13c1d262542b8293e646fe3c2cd46eeb
robertblank <robert.blank@oxid-esales.com>
2016-05-24 17:30:26 +0200

    ESDEV-3641 Remove isValidFieldName from Database class.
    This method was used to "validate" an user provided value to use it later in a SQL query as the column name of the "order by" clause.
    Field name validation is not done anymore as it was a very weak validation anyway and it would be too expensive to do a real , database platform independent validation. It would have to take into account reserved words, etc ...
    Instead the column name part of "order by" clause is now quoted with DatabaseInterface::quoteIdentfier(), in BaseController::getSortingSql(), which is much more secure, as this string could always be safely (but maybe not successfully) used in a SQL query as column name in the "order by" clause.
    (cherry picked from commit f6cf31a)

--- source/Application/Controller/BaseController.php
+++ source/Application/Controller/BaseController.php
@@ -912,8 +912,7 @@ class BaseController extends \oxView
         if ($sortBy &&
             $sortOrder &&
             Registry::getUtils()->isValidAlpha($sortOrder) &&
-            in_array(Str::getStr()->strtolower($sortOrder), $sortDirections) &&
-            in_array($sortBy, oxNew('oxArticle')->getFieldNames())
+            in_array(Str::getStr()->strtolower($sortOrder), $sortDirections)
         ) {
             $sorting = array('sortby' => $sortBy, 'sortdir' => $sortOrder);
         }
